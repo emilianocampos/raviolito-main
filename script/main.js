78c22f90d93aba833c73d206c8b1470f930e7e96
emilianocampos <emicampos1268@gmail.com>
2024-04-26 22:43:28 -0300

    2

--- script/main.js
+++ script/main.js
@@ -1,12 +1,1 @@
 AOS.init();
-var typed = new Typed('#element', {
-    strings: ['El Raviolito - Pastas Caseras'],
-    typeSpeed: 40,
-  });
-
-const textoParrafo = document.querySelectorAll('.pasta-texto');
-
-
-
-
-console.log(textoParrafo);
